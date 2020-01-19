edc31fe5fbbe3199272bc99323a5d46628a609e8
lobana alarabi <loubanaeng@gmail.com>
2020-01-19 16:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const removeLast=["Milo", "Otis", "Garfield"];
 const removeFirst=["Milo", "Otis", "Garfield"];
 append.push("Odie");
 prepend.unshift("Odie");
-removeLast.pop("Odie");
-removeFirst.shift("Odie");
+removeLast.pop();
+removeFirst.shift();
